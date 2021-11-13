063487ff4187ac9d61cf9bb2cf6a15bc7c64426a
Savio <andradeasavio9073@gmail.com>
2021-11-13 16:30:42 +0000

    finished MagneticField around Wire scene

--- MagneticFields.py
+++ MagneticFields.py
@@ -1,9 +1,14 @@
 from manim import *
 from manim_physics import *
 
-class MagnetismExample(MovingCameraScene):
+class MagneticFieldAroundWire(ThreeDScene):
     def construct(self):
-        current1 = Current()
-        field = CurrentMagneticField(current1)
+        current1 = Current(magnitude=10)
+        _field = CurrentMagneticField(current1)
+        field = VGroup(_field)
         self.add(current1, field)
-        self.play(field.animate.rotate(1), run_time=5, rate_func=rate_functions.sigmoid)
+        self.begin_ambient_camera_rotation(rate=-0.3)
+        self.wait(5.2)
+        self.stop_ambient_camera_rotation()
+
+#        self.play(field.animate.rotate(90*DEGREES, about_point=ORIGIN), run_time=1, rate_func=rate_functions.linear)
