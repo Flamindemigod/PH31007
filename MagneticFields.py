7a9f6eebb649e3cae05e8eff48e8a08246eedd77
Savio <andradeasavio9073@gmail.com>
2021-11-13 16:34:48 +0000

    cleaned up

--- MagneticFields.py
+++ MagneticFields.py
@@ -4,8 +4,7 @@ from manim_physics import *
 class MagneticFieldAroundWire(ThreeDScene):
     def construct(self):
         current1 = Current(magnitude=10)
-        _field = CurrentMagneticField(current1)
-        field = VGroup(_field)
+        field = CurrentMagneticField(current1)
         self.add(current1, field)
         self.begin_ambient_camera_rotation(rate=-0.3)
         self.wait(5.2)
