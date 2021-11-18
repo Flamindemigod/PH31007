ba96bd2bd003fda68a50659dcf2f3b9bddd64775
Savio <andradeasavio9073@gmail.com>
2021-11-18 13:31:08 +0000

    made version of the capacitor animations and magnetic field

--- CapacitorPlates.py
+++ CapacitorPlates.py
@@ -5,7 +5,7 @@ import random
  """
 #TODO
 ## Fade the vectors a bit faster
-class CapacitorPlates(ThreeDScene):
+class CapacitorwithEfield(ThreeDScene):
     def construct(self):
         self.set_camera_orientation(phi=15 * DEGREES, theta=-45 * DEGREES, gamma=45 * DEGREES)
         CapPlate1 = Rectangle(height=2, width=2, color=BLUE_B,grid_xstep=0.1, grid_ystep=0.1).rotate(90*DEGREES, axis=Y_AXIS)
@@ -30,30 +30,43 @@ class CapacitorPlates(ThreeDScene):
             vec.add(Vector(CapPlate2.get_center()-CapPlate1.get_center(), buff=0.3).shift(i*DOWN/10).shift(random.random()*0.5*LEFT))
 
         self.play(FadeIn(vec),
-                vec.animate.shift(0.5*RIGHT), rate_func=rate_functions.linear)
-        self.play(FadeOut(vec, shift=0.5*RIGHT), rate_func=rate_functions.linear)
-        return
+                vec.animate.shift(0.4*RIGHT), rate_func=rate_functions.linear)
+        self.play(FadeOut(vec, shift=0.2*RIGHT), rate_func=rate_functions.linear)
+        wire_current_group.move_to(wire2).shift(1.5*LEFT).shift(0.25*DOWN)
         
-        #Wire
-        wire = Line(4*UP, 4*DOWN).set_color(BLUE_C)
-        wire_current = Cone(direction=Y_AXIS, show_base=False, base_radius=0.06, height=0.09).shift(4*DOWN)
-        wire_current_label = Tex("I").move_to(wire_current).shift(0.5*LEFT)
+        self.play(FadeIn(wire_current_group),
+                wire_current_group.animate.shift(1.5*RIGHT), run_time=1.5, rate_func=rate_functions.linear)
+        self.play(FadeOut(wire_current_group, shift = (1.5*RIGHT)), run_time=1.5, rate_func=rate_functions.linear)
+
+class CapacitorwithnoEfield(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=15 * DEGREES, theta=-45 * DEGREES, gamma=45 * DEGREES)
+        CapPlate1 = Rectangle(height=2, width=2, color=BLUE_B,grid_xstep=0.1, grid_ystep=0.1).rotate(90*DEGREES, axis=Y_AXIS)
+        CapPlate2 = Rectangle(height=2, width=2, color=BLUE_C,grid_xstep=0.1, grid_ystep=0.1).rotate(90*DEGREES, axis=Y_AXIS).shift(RIGHT)
+        
+        CapPlates = VGroup(CapPlate1, CapPlate2)
+
+        wire1 = Line(3*LEFT, [0,0,0]).set_color(BLUE_B)
+        wire2 = Line(RIGHT, 4*RIGHT).set_color(BLUE_C)
+        wire_current = Cone(direction=X_AXIS, show_base=False, base_radius=0.06, height=0.09).shift(3*LEFT)
+        wire_current_label = Tex("I").move_to(wire_current).shift(0.5*DOWN).scale(0.4)
         wire_current_group = VGroup(wire_current, wire_current_label)
-        wire_group = VGroup(wire, wire_current_group).shift(6*RIGHT)
-        self.add(wire_group)
 
-        #Magnet
-        MagnetNorth = Polygon(0.3*LEFT, 0.3*RIGHT, UP, fill_opacity=1).set_color(RED_A)
-        MagnetNorthLabel = Tex("N").shift(0.3*UP).set_color(RED_D)
-        MagnetSouth = Polygon(0.3*LEFT, 0.3*RIGHT, DOWN, fill_opacity=1).set_color(BLUE_A)
-        MagnetSouthLabel = Tex("S").shift(0.3*DOWN).set_color(BLUE_D)
-        Magnet = VGroup(MagnetNorth, MagnetSouth, MagnetNorthLabel, MagnetSouthLabel)
-        self.add(Magnet)
+        self.play(FadeIn(CapPlates, wire1, wire2))
+        self.play(FadeIn(wire_current_group),
+                wire_current_group.animate.shift(1.5*RIGHT), run_time=1.5, rate_func=rate_functions.linear)
+        self.play(FadeOut(wire_current_group, shift = (1.5*RIGHT)), run_time=1.5, rate_func=rate_functions.linear)
 
+        #vec=VGroup()
+        #for i in range(10):
+        #    vec.add(Vector(CapPlate2.get_center()-CapPlate1.get_center(), buff=0.3).shift(i*UP/10).shift(random.random()*0.5*LEFT))
+        #    vec.add(Vector(CapPlate2.get_center()-CapPlate1.get_center(), buff=0.3).shift(i*DOWN/10).shift(random.random()*0.5*LEFT))
+
+        #self.play(FadeIn(vec),
+        #        vec.animate.shift(0.4*RIGHT), rate_func=rate_functions.linear)
+        #self.play(FadeOut(vec, shift=0.2*RIGHT), rate_func=rate_functions.linear)
+        wire_current_group.move_to(wire2).shift(1.5*LEFT).shift(0.25*DOWN)
         
-        self.play(
-            Magnet.animate.rotate(-0.5),
-            wire_current_group.animate.shift(4*UP), run_time=2.5, rate_func=rate_functions.linear)
-        self.play(
-            Magnet.animate.rotate(0.5),
-            wire_current_group.animate.shift(4*UP), run_time=2.5, rate_func=rate_functions.linear)
+        self.play(FadeIn(wire_current_group),
+                wire_current_group.animate.shift(1.5*RIGHT), run_time=1.5, rate_func=rate_functions.linear)
+        self.play(FadeOut(wire_current_group, shift = (1.5*RIGHT)), run_time=1.5, rate_func=rate_functions.linear)
--- MagneticFields.py
+++ MagneticFields.py
@@ -1,5 +1,6 @@
 from manim import *
 from manim_physics import *
+import random
 
 class MagneticFieldAroundWire(ThreeDScene):
     def construct(self):
@@ -26,3 +27,57 @@ class ArtisticMagneticFieldAroundWire(ThreeDScene):
             FadeIn(field), run_time = 2)
         self.wait(8.4)
         self.stop_ambient_camera_rotation()
+
+class CapacitorMagneticField(ThreeDScene):
+    def construct(self):
+        config.frame_height = 30
+        config.frame_width = 30
+        self.set_camera_orientation(phi=40 * DEGREES, theta=-30 * DEGREES, gamma=40 * DEGREES)
+        #self.set_camera_orientation(phi=90 * DEGREES)
+        
+        CapPlate1 = Rectangle(height=4, width=4, color=BLUE_B,grid_xstep=0.1, grid_ystep=0.1).rotate(90*DEGREES, axis=Y_AXIS)
+        CapPlate2 = Rectangle(height=4, width=4, color=BLUE_C,grid_xstep=0.1, grid_ystep=0.1).rotate(90*DEGREES, axis=Y_AXIS).shift(RIGHT)
+        
+        CapPlates = VGroup(CapPlate1, CapPlate2)
+
+        wire1 = Line(10*LEFT, [0,0,0]).set_color(BLUE_B)
+        wire2 = Line(RIGHT, 15*RIGHT).set_color(BLUE_C)
+        capacitor = VGroup(CapPlates,wire2,wire1).rotate_about_origin(-90*DEGREES, axis=Y_AXIS)
+        current = Current(magnitude=10)
+        field = CurrentMagneticField(current)
+        self.begin_ambient_camera_rotation(rate=-0.2)
+        self.play(
+            Write(capacitor, reverse=True, rate_func=rate_functions.smooth),
+            FadeIn(field), run_time = 2)
+        for _ in range(5):
+            vec=VGroup()
+            _vec_range = 50
+            for i in range(_vec_range):
+                vec.add(Vector(CapPlate1.get_center()-CapPlate2.get_center(), buff=0.3).shift(i*UP/(_vec_range/2)).shift(random.random()*1.8*LEFT))
+                vec.add(Vector(CapPlate1.get_center()-CapPlate2.get_center(), buff=0.3).shift(i*UP/(_vec_range/2)).shift(random.random()*1.8*RIGHT))
+                
+                vec.add(Vector(CapPlate1.get_center()-CapPlate2.get_center(), buff=0.3).shift(i*DOWN/(_vec_range/2)).shift(random.random()*1.8*LEFT))
+                vec.add(Vector(CapPlate1.get_center()-CapPlate2.get_center(), buff=0.3).shift(i*DOWN/(_vec_range/2)).shift(random.random()*1.8*RIGHT))
+
+            vec.shift(OUT*1.2)
+            self.play(FadeIn(vec),
+                    vec.animate.shift(0.4*IN), rate_func=rate_functions.linear, run_time=0.6*3)
+            self.play(FadeOut(vec, shift=0.2*IN), rate_func=rate_functions.linear, run_time=0.3*3)
+            self.wait(0.5)
+        self.stop_ambient_camera_rotation()
+
+class DisplayCurl(Scene):
+    def construct(self):
+        MagneticField = Circle(radius=1, color=RED_B)
+        MagneticFieldLabel = MathTex(r"\vec{B}.\vec{dl}")
+        MagField = VGroup(MagneticField, MagneticFieldLabel).arrange(DOWN)
+
+        
+        CurlMagneticField = Circle(radius=1, color=BLUE_B, fill_opacity=0.3)
+        CurlMagneticFieldLabel = MathTex(r"\nabla\times\textbf{B}")
+        CurlMagField = VGroup(CurlMagneticField, CurlMagneticFieldLabel).arrange(DOWN)
+
+        VGroup(MagField, CurlMagField).arrange(RIGHT, buff=2)
+        self.play(Write(MagField), Write(CurlMagField), run_time = 3, rate_func=rate_functions.smooth)
+
+        
