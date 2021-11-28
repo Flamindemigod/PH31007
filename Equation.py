d4560a384241f21c735826d27b330e4fea038927
Savio <andradeasavio9073@gmail.com>
2021-11-28 01:45:25 +0000

    added maxwells eqns and ame diff form

--- Equation.py
+++ Equation.py
@@ -63,3 +63,106 @@ class StokesTheorem(Scene):
             Unwrite(title))
         self.wait()
         return
+
+class AMEDiffren(Scene):
+    def construct(self):
+        title = Tex("Ampere-Maxwell equation (Integral Form)")
+        AMEIntegral = MathTex(r"\oint_C \vec{\mathbf{b}} \cdot \vec{\mathbf{dl}}", r"=",  r"\mu_0 I_{enc} + \mu_0 \varepsilon_0 \frac{d\Phi_E}{dt}")
+        AMEIntegralIntermediate = MathTex(r"\oint_C \vec{\mathbf{b}} \cdot \vec{\mathbf{dl}}", r"=",  r"\iint_{\mathbf{S}}\mu_0 \mathbf{J}\cdot d\mathbf{S} + \iint_{\mathbf{S}}\mu_0 \varepsilon_0 \frac{\delta \mathbf{E}}{\delta t}\cdot d\mathbf{S}")
+        AMEIntegralDensityVersion = MathTex(r"\oint_C \vec{\mathbf{b}} \cdot \vec{\mathbf{dl}}", r"=",  r"\iint_{\mathbf{S}}(\mu_0 \mathbf{J} + \mu_0 \varepsilon_0 \frac{\delta \mathbf{E}}{\delta t})\cdot d\mathbf{S}")
+        AMEDiffrentialDensityVersion = MathTex(r"\nabla \times \mathbf{B}", r"=",  r"\mu_0 \mathbf{J} + \mu_0 \varepsilon_0 \frac{\delta\mathbf{E}}{\delta t}")
+        AMEDiffrentialDensityVersionSeperated = MathTex(r"\nabla \times \mathbf{B}", r"=",  r"\mu_0",r"\mathbf{J}", "+",r"\mu_0 \varepsilon_0", r"\frac{\delta\mathbf{E}}{\delta t}")
+
+        VGroup(title, VGroup(AMEIntegral, AMEIntegralIntermediate, AMEIntegralDensityVersion, AMEDiffrentialDensityVersion, AMEDiffrentialDensityVersionSeperated)).arrange(DOWN)
+        AMEHighlightJ = Brace(AMEDiffrentialDensityVersionSeperated[3], buff = .1)
+        AMEHighlightJLabel = AMEHighlightJ.get_text("Current Density").scale(0.5)
+        AMEHighlightE = Brace(AMEDiffrentialDensityVersionSeperated[-1], buff = .1)
+        AMEHighlightELabel = AMEHighlightE.get_text("Change in Electric Field").scale(0.5)
+
+        self.play(Write(title))
+        self.play(FadeIn(AMEIntegral, shift=UP))
+        self.wait()
+        self.play(Transform(AMEIntegral, AMEIntegralIntermediate))
+        self.wait()
+        self.play(Transform(AMEIntegral, AMEIntegralDensityVersion))
+        self.wait()
+        self.play(Transform(AMEIntegral, AMEDiffrentialDensityVersion), title.animate.become(Tex("Ampere-Maxwell equation (Diffrential Form)").move_to(title)))
+        self.wait()
+        self.play(Create(VGroup(AMEHighlightJ, AMEHighlightJLabel, AMEHighlightE, AMEHighlightELabel)))
+        self.wait()
+        self.play(Uncreate(VGroup(AMEHighlightJ, AMEHighlightJLabel, AMEHighlightE, AMEHighlightELabel)))
+        self.play(FadeOut(AMEIntegral, shift=DOWN))
+        self.play(Unwrite(title))
+        self.wait()
+
+class Maxwell1(Scene):
+    def construct(self):
+        Eqn = MathTex(r"\nabla \cdot \mathbf{E} = \frac{\rho}{\varepsilon_0}")
+        title = Tex(r"Gauss's Law for Static Electric Fields")
+        VGroup(Eqn, title).arrange(UP)
+
+        
+        self.play(
+            Write(title)
+        )
+        self.play(FadeIn(Eqn, shift=UP))
+        self.wait(2)
+        self.play(
+            FadeOut(Eqn),
+            Unwrite(title))
+        self.wait()
+        return
+
+class Maxwell2(Scene):
+    def construct(self):
+        Eqn = MathTex(r"\nabla \cdot \mathbf{B} = 0")
+        title = Tex(r"Gauss's Law for Static Magnetic Fields")
+        VGroup(Eqn, title).arrange(UP)
+
+        
+        self.play(
+            Write(title)
+        )
+        self.play(FadeIn(Eqn, shift=UP))
+        self.wait(2)
+        self.play(
+            FadeOut(Eqn),
+            Unwrite(title))
+        self.wait()
+        return
+
+class Maxwell3(Scene):
+    def construct(self):
+        Eqn = MathTex(r"\nabla \times \mathbf{E} = \frac{\delta\mathbf{B}}{\delta t}")
+        title = Tex(r"Faraday's Law of Induction")
+        VGroup(Eqn, title).arrange(UP)
+
+        
+        self.play(
+            Write(title)
+        )
+        self.play(FadeIn(Eqn, shift=UP))
+        self.wait(2)
+        self.play(
+            FadeOut(Eqn),
+            Unwrite(title))
+        self.wait()
+        return
+
+class Maxwell4(Scene):
+    def construct(self):
+        Eqn = MathTex(r"\nabla \times \mathbf{B} = \mu_0 \mathbf{J} + \mu_0 \varepsilon_0 \frac{\delta\mathbf{E}}{\delta t}")
+        title = Tex(r"Ampere-Maxwell Law")
+        VGroup(Eqn, title).arrange(UP)
+
+        
+        self.play(
+            Write(title)
+        )
+        self.play(FadeIn(Eqn, shift=UP))
+        self.wait(2)
+        self.play(
+            FadeOut(Eqn),
+            Unwrite(title))
+        self.wait()
+        return
